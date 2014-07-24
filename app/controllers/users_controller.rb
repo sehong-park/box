049a732329dceff4ff49b6669569220e418b8993
Sehong Park <sehong.park86@gamil.com>
2014-07-24 18:24:16 +0000

    user CRUD

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: [:destroy]
   
   def new
     @user = User.new
@@ -23,7 +24,7 @@ class UsersController < ApplicationController
   end
   
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.paginate(page: params[:page], per_page: 10)
   end
   
   def edit
@@ -38,6 +39,12 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted."
+    redirect_to users_url
   end
   
   private #########################################################
@@ -61,5 +68,8 @@ class UsersController < ApplicationController
       redirect_to(root_url) unless current_user?(@user)
     end
   
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
   ###################################################################
 end
