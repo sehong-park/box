de5182bfa51aab363aff42dbb9fed50c0b6fe5b9
Sehong Park <sehong.park86@gamil.com>
2014-07-30 05:13:23 +0000

    ordering...

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,17 @@
 class OrdersController < ApplicationController
   
   def new
-    
+    @order = Order.new
+  end
+  
+  def create
+    @order = User.new(order_params)
+    if @order.save
+      flash[:success] = "New order created!"
+      redirect_to @order
+    else
+      flash[:warning] = "Ordering failed.."
+      render 'new'
+    end
   end
 end
