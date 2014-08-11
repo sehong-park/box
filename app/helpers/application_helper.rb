751bc9e9c61d6dd1b81a61a7159a691c9b5fa7d2
Sehong Park <sehong.park86@gamil.com>
2014-08-11 11:41:35 +0000

    updating.. main page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,8 @@ module ApplicationHelper
       "#{base_title} | #{page_title}"
     end
   end
+  
+  def icon(name, text)
+    "<span class='glyphicon glyphicon-#{name}'></span>".html_safe<<" #{text}"
+  end
 end
