ae15614f6c7e16c9de228b65b5d3559cbbffbd7c
Sehong Park <sehong.park86@gamil.com>
2014-08-18 07:10:38 +0000

    debuging pricing problem.. with non-login-user

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-  before_action :signed_in_user
+  before_action :signed_in_user, except: [:pricing]
   before_action :owner_or_admin, only: [:show, :edit, :update, :delete]
   
   include OrdersHelper
