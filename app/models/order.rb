aabb81bb974c695310d2a9c1b52ee1595b5b07d9
Sehong Park <sehong.park86@gamil.com>
2014-09-03 11:17:55 +0000

    editing order meeting time

--- app/models/order.rb
+++ app/models/order.rb
@@ -13,6 +13,7 @@ class Order < ActiveRecord::Base
   WHY_ORDERING = ["물품보관","이사", "여행", "출국", "기타"] 
   
   LOCATION = [["서울", 0], ["경기", 7500] , ["인천공항", 45000]]
+  AVAILABLE_MEETING_TIMES = [["10-13",10], ["13-16", 13], ["16-19", 16], ["19-21", 19]]
   
   validates :user_id, presence: true
   validates :unit_count, presence: true, numericality: { greater_than: 0 }
