1a1cfa25f35f9303c603666edbd7eed73afb9b55
Sehong Park <sehong.park86@gamil.com>
2014-09-03 11:23:01 +0000

    wef

--- app/models/order.rb
+++ app/models/order.rb
@@ -13,7 +13,7 @@ class Order < ActiveRecord::Base
   WHY_ORDERING = ["물품보관","이사", "여행", "출국", "기타"] 
   
   LOCATION = [["서울", 0], ["경기", 7500] , ["인천공항", 45000]]
-  AVAILABLE_MEETING_TIMES = [["10-13",10], ["13-16", 13], ["16-19", 16], ["19-21", 19]]
+  AVAILABLE_MEETING_TIMES = [["10시-13시",10], ["13시-16시", 13], ["16시-19시", 16], ["19시-21시", 19]]
   
   validates :user_id, presence: true
   validates :unit_count, presence: true, numericality: { greater_than: 0 }
